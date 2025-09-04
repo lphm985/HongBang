7959905ae05aed37ac272d63f2ac825a2523b963
lphm985 <lphminhvuive@gmail.com>
2025-09-04 16:46:34 +0700

    Mô tả thay đổi

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,14 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-    host: '127.0.0.1', 
+    host: 'https://hongbang.onrender.com', 
     port: 3306,
     user: 'lphm985', 
     password: 'Lphm123',
     database: 'hong_bang',
-    connectionLimit: 10,
-    idleTimeout: 60000, 
-    acquireTimeout: 60000,
+    connectionLimit: 20,
+    idleTimeout: 120000, 
+    acquireTimeout: 120000,
     // Trả về các số lớn (BIGINT) dưới dạng chuỗi để tránh lỗi JSON serialization
     supportBigNumbers: true,
     bigNumberStrings: true,
