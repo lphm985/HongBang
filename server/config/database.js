f80b551325e675bd871708dffce36263c7d168aa
lphm985 <lphminhvuive@gmail.com>
2025-09-04 16:15:50 +0700

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-    host: 'localhost', 
+    host: '127.0.0.1', 
     port: 3306,
     user: 'lphm985', 
     password: 'Lphm123',
