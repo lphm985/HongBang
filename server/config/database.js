6d070a1b83d2b4bc9e05e462423677d354cc0391
lphm985 <lphminhvuive@gmail.com>
2025-09-05 23:44:11 +0700

    first commit

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,14 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-    host: 'https://hongbang.onrender.com', 
+    host: 'localhost', 
     port: 3306,
-    user: 'lphm985', 
-    password: 'Lphm123',
-    database: 'hong_bang',
-    connectionLimit: 20,
-    idleTimeout: 120000, 
-    acquireTimeout: 120000,
+    user: 'root', 
+    password: 'Vhuu@2002',
+    database: 'tu_tien_db',
+    connectionLimit: 10,
+    idleTimeout: 60000, 
+    acquireTimeout: 60000,
     // Trả về các số lớn (BIGINT) dưới dạng chuỗi để tránh lỗi JSON serialization
     supportBigNumbers: true,
     bigNumberStrings: true,
